37473685154ebdca13426b2dfc3c3fefa14123fd
Anderson_Ramos_07 <andersonramos07@live.com>
2024-10-02 20:56:08 -0300

    adjusting Main

--- project_java_anderson/src/Main.java
+++ project_java_anderson/src/Main.java
@@ -6,6 +6,9 @@ public class Main {
 		double x = 10.358923;
 		System.out.println("Hello World!");
 		System.out.printf("%.2f%n", x);
+		
+		Exercicio1 exercicio = new Exercicio1();
+		exercicio.main(null);
 	}
 
 }
