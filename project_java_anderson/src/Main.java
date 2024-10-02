71237ec865b0c73f1169e9e95a9f82e6e6543956
Anderson_Ramos_07 <andersonramos07@live.com>
2024-10-01 23:00:57 -0300

    starting Java course

---
+++ project_java_anderson/src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double x = 10.358923;
+		System.out.println("Hello World!");
+		System.out.printf("%.2f%n", x);
+	}
+
+}
