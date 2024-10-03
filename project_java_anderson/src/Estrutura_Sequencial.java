1a2b471d3598c3fb037e7c84279eceaefd8deef7
Anderson_Ramos_07 <andersonramos07@live.com>
2024-10-02 21:13:26 -0300

    exercise 3

--- project_java_anderson/src/Estrutura_Sequencial.java
+++ project_java_anderson/src/Estrutura_Sequencial.java
@@ -5,7 +5,8 @@ public class Estrutura_Sequencial {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 //		exe01();
-		exe02();
+//		exe02();
+		exe03();
 	}
 	public static void exe01() {
 		int number1, number2;
@@ -14,10 +15,10 @@ public class Estrutura_Sequencial {
 		Scanner scan = new Scanner(System.in);
 		number1 = scan.nextInt();
 		number2 = scan.nextInt();
+		scan.close();
 		soma = number1 + number2;
 		System.out.println("SOMA = "+soma);
 	}
-
 
 	public static void exe02() {
 		double raio, pi, result;
@@ -28,6 +29,19 @@ public class Estrutura_Sequencial {
 		raio = scan.nextDouble();
 		result = pi * Math.pow(raio, 2);
 		System.out.printf("A = %.4f",result);
+	}
+	
+	public static void exe03() {
+		int A, B, C, D, diferenca;
+	
+		Scanner scan = new Scanner(System.in);
+		A = scan.nextInt();
+		B = scan.nextInt();
+		C = scan.nextInt();
+		D = scan.nextInt();
+		scan.close();
+		diferenca = (A*B - C*D);
+		System.out.print("DIFERENCA = " + diferenca);
 	}
 
 }
