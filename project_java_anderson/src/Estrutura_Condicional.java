8d50e0ed9cc37a707d393f96b2432abf94bcc2c5
Anderson_Ramos_07 <andersonramos07@live.com>
2024-10-05 19:18:41 -0300

    exercise2 & exercise3

--- project_java_anderson/src/Estrutura_Condicional.java
+++ project_java_anderson/src/Estrutura_Condicional.java
@@ -4,7 +4,9 @@ public class Estrutura_Condicional {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		exe01();
+//		exe01();
+//		exe02();
+		exe03();
 	}
 	public static void exe01()
 	{
@@ -15,5 +17,29 @@ public class Estrutura_Condicional {
 			System.out.println("NAO NEGATIVO");
 		else
 			System.out.println("NEGATIVO");
+		scan.close();
 	}
+	public static void exe02()
+	{
+		int number = 0;
+		Scanner scan = new Scanner(System.in);
+		number = scan.nextInt();
+		if(number % 2 == 0)
+			System.out.println("PAR");
+		else
+			System.out.println("IMPAR");
+		scan.close();
+	}
+	public static void exe03()
+	{
+		int number1, number2;
+		Scanner scan = new Scanner(System.in);
+		number1 = scan.nextInt();
+		number2 = scan.nextInt();
+		if(number1 % number2 == 0 || number2 % number1 == 0)
+			System.out.println("Sao Multiplos");
+		else
+			System.out.println("Nao sao multiplos");
+	}
+
 }
