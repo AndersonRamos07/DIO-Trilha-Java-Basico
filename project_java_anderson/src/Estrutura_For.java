a2271b2d8825c0b2cd443c049a98c00c616339fb
Anderson_Ramos_07 <andersonramos07@live.com>
2024-10-23 20:40:52 -0300

    adding Estrutura For - exercise 1

---
+++ project_java_anderson/src/Estrutura_For.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class Estrutura_For {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		exe01();
+//		exe02();
+//		exe03();
+//		exe04();
+//		exe05();
+//		exe06();
+//		exe07();
+	}
+	public static void exe01()
+	{
+		int number;		
+		Scanner scan = new Scanner(System.in);		
+		number = scan.nextInt();
+		for(int i = 0; i < number || i == number; i++) {
+			if(i % 2 == 1) {
+				System.out.println(i);
+			}
+		}
+	}
+	public static void exe02()
+	{
+		
+	}
+	public static void exe03()
+	{
+		
+	}
+	public static void exe04()
+	{
+		
+	}
+	public static void exe05()
+	{
+		
+	}
+	public static void exe06()
+	{
+		
+	}
+	public static void exe07()
+	{
+		
+	}
+}
