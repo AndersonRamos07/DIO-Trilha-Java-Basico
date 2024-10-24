c2b52c6b0749c569a5a095f74da32530873c48d1
Anderson_Ramos_07 <andersonramos07@live.com>
2024-10-23 21:14:56 -0300

    exercise 2

--- project_java_anderson/src/Estrutura_For.java
+++ project_java_anderson/src/Estrutura_For.java
@@ -4,8 +4,8 @@ public class Estrutura_For {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		exe01();
-//		exe02();
+//		exe01();
+		exe02();
 //		exe03();
 //		exe04();
 //		exe05();
@@ -21,11 +21,26 @@ public class Estrutura_For {
 			if(i % 2 == 1) {
 				System.out.println(i);
 			}
+		scan.close();
 		}
 	}
 	public static void exe02()
 	{
-		
+		int quantidade, numInOut;
+		int nIm = 0;
+		int nOut =0;		
+		Scanner scan = new Scanner(System.in);		
+		quantidade = scan.nextInt();
+		for(int i = 0; i < quantidade; i++)
+		{
+			numInOut = scan.nextInt();
+			if(numInOut >= 10 && numInOut <= 20)
+				nIm++;
+			else
+				nOut++;
+		}
+		scan.close();
+		System.out.println(nIm + " in \n"+nOut + " out");
 	}
 	public static void exe03()
 	{
