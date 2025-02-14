ddb08c2bdf0f46559255ec12526ac67e76f00093
Anderson_Ramos_07 <andersonramos07@live.com>
2025-02-14 12:51:07 -0300

    feat: adicionando linhas de codigo para o curso dio

--- project_java_anderson/src/Main.java
+++ project_java_anderson/src/Main.java
@@ -1,11 +1,29 @@
+import curso_dio.SmartTv;
 
-public class Main {
+public class Main {	//Usuario
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception{
 		// TODO Auto-generated method stub
-		double x = 10.358923;
-		System.out.println("Hello World!");
-		System.out.printf("%.2f%n", x);
+
+		SmartTv smartTv = new SmartTv();
+
+		smartTv.diminuirVolume();
+		smartTv.diminuirVolume();
+		smartTv.diminuirVolume();
+		smartTv.aumentarVolume();
+
+		smartTv.mudarCanal(46);
+
+		System.out.println("TV Ligada? "+smartTv.ligada);
+		System.out.println("Canal Atual: "+smartTv.canal);
+		System.out.println("Volume Atual: "+smartTv.volume);
+
+		smartTv.ligar();
+		System.out.println("Novo Status => TV Ligada? "+smartTv.ligada);
+
+//		double x = 10.358923;
+//		System.out.println("Hello World!");
+//		System.out.printf("%.2f%n", x);
 		
 //		Exercicio1 exercicio = new Exercicio1();
 //		exercicio.main(null);
