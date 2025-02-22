19c746953481dcbb12b0477348962c24695afcba
Anderson_Ramos_07 <andersonramos07@live.com>
2025-02-22 13:28:11 -0300

    build: Criando projeto para os Exercicios - Java e a Arte da Abstração com Classes e Encapsulamento

---
+++ Exercicios/src/ContaBancaria.java
@@ -0,0 +1,15 @@
+public class ContaBancaria {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
+
+//todo
+/*
+ *  - Consultar saldo
+    - consultar cheque especial
+    - Depositar dinheiro;
+    - Sacar dinheiro;
+    - Pagar um boleto.
+    - Verificar se a conta está usando cheque especial.
+ */
