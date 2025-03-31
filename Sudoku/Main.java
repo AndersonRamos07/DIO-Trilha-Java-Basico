a54e997796df9ff3d0c328b0daef8d601e008907
Anderson Ramos <andersonramos07@live.com>
2025-03-31 19:36:02 -0400

    feat: pausando a criacao de um Sudoku e clonando o repositorio da DIO, alterando o Board principal para o BOARD_TEMPLATE2

---
+++ Sudoku/Main.java
@@ -0,0 +1,8 @@
+import java.awt.Dimension;
+
+public class Main{
+    public static void main(String[] args) {
+        var dimensao = new Dimension(360,360);
+        JFrame myNFrame = new myFrame(dimensao, myPainel);
+    }
+}
