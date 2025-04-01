82300d85c415c4514d2f81530f1f7233f84de960
Anderson Ramos <andersonramos07@live.com>
2025-03-31 21:32:14 -0400

    build: Criando um projeto para o Desafio de Padroes de Projeto

---
+++ DesafioPadroesDeProjeto/padroes-de-projeto-desafio-dio/src/main/java/com/andersonros07/desafio_dio_padroes_de_projeto/padroes_de_projeto_desafio_dio/PadroesDeProjetoDesafioDioApplication.java
@@ -0,0 +1,13 @@
+package com.andersonros07.desafio_dio_padroes_de_projeto.padroes_de_projeto_desafio_dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PadroesDeProjetoDesafioDioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PadroesDeProjetoDesafioDioApplication.class, args);
+	}
+
+}
